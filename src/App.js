a52dd98f69c782e16e325ab753c9757cdbdd5480
真下清 <mashimo.kiyoshi>
2019-09-23 20:52:56 +0900

    add message

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>↑ I tried it and I can make.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
